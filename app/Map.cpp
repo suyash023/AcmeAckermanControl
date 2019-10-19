a186b3ee69bd9717c4dda44ab513a141b746de73
nakulpatel94 <nakulp123@gmail.com>
2019-10-19 03:20:20 -0400

    Unique ID:2.01. Addded the implementation of all methods for Map class

--- app/Map.cpp
+++ app/Map.cpp
@@ -42,7 +42,6 @@
  * @param inputCoordinates Coordinates specified by user
  * @return bool true or false depending on if the value was set or not
  **/
-
 bool Map::SetStartCoordinates(cv::Point3f inputCoordinates) {
   bool b = UpdateRobotLocation(inputCoordinates);
   return b;
@@ -73,9 +72,47 @@ bool Map::SetDestinationCoordinates(cv::Point3f inputCoordinates) {
 bool Map::UpdateRobotLocation(cv::Point3f coordinates) {
   bool val = CheckValidCoordinates(coordinates);
   if (val) {
+    currCoords = coordinates;
+    cv::Point2f center(coordinates.x, coordinates.y);
+    cv::Size2f robotSize(length, width);
+    float angle = coordinates.z;
+    /// creating a rotated rectangle for updated position
+    cv::RotatedRect rotatedRectangle = cv::RotatedRect(center, robotSize,
+                                                       angle);
+    std::vector<cv::Point2f> rectPoints;
+    std::vector<cv::Point2f> rotatedPoints;
+    rectPoints.push_back(cv::Point2f(-length / 2, -width / 2));
+    rectPoints.push_back(cv::Point2f(length / 2, -width / 2));
+    rectPoints.push_back(cv::Point2f(length / 2, width / 2));
+    rectPoints.push_back(cv::Point2f(-length / 2, width / 2));
+    cv::Point2f translationPoint;
+    translationPoint.x = center.x * cos(angle * 3.14 / 180)
+        + center.y * sin(angle * 3.14 / 180);
+    translationPoint.y = -center.x * sin(angle * 3.14 / 180)
+        + center.y * cos(angle * 3.14 / 180);
 
+    for (auto p : rectPoints) {
+      cv::Point2f rotPoint;
+      rotPoint.x = p.x * cos(angle * M_PI / 180) - p.y * sin(angle * M_PI / 180)
+          + center.x;
+      rotPoint.y = p.x * sin(angle * M_PI / 180) + p.y * cos(angle * M_PI / 180)
+          + center.y;
+      rotatedPoints.push_back(rotPoint);
+    }
+    std::vector<cv::Point> corners;
+    std::transform(rotatedPoints.begin(), rotatedPoints.begin(),
+                   std::back_inserter(corners),
+                   [](const cv::Point2f& p) {return (cv::Point)p;});
 
-
+    for (auto p : rotatedPoints) {
+      corners.push_back(p);
+    }
+    /// using fillConvexPoly to fill rectangle with color
+    cv::fillConvexPoly(currMapImage, corners.data(), 4,
+                       cv::Scalar(0, 255, 0));
+    std::cout << "Corners: " << corners << std::endl;
+    bool b = DisplayMapImage();
+    return true;
   } else {
     std::cout << "Could not update the coordinates." << std::endl;
     return false;
@@ -92,6 +129,7 @@ cv::Point3f Map::GetRobotCoordinates() {
   return currCoords;
 }
 
+
 /**
  * @brief function to initialize the map in which robot will move.
  * Robot image is loaded and stored in robot image
@@ -100,9 +138,23 @@ cv::Point3f Map::GetRobotCoordinates() {
  * @param none
  * @return Mat initialized map image with robot at one corner.
  */
-
 bool Map::InitializeMap(cv::Point inMapBounds, double robotLength,
-        double robotWidth) {
+                        double robotWidth) {
+  length = robotLength * 10.0;
+  width = robotWidth * 10.0;
+  int bound = 1000;
+  if (inMapBounds.x < bound && inMapBounds.y < bound && inMapBounds.x > 0
+      && inMapBounds.y > 0) {
+    mapBounds.x = inMapBounds.x;
+    mapBounds.y = inMapBounds.y;
+    currMapImage = cv::Mat::zeros(mapBounds.y, mapBounds.x, CV_8UC3);
+    currMapImage.setTo(cv::Scalar(255, 255, 255));
+    UpdateRobotLocation(
+        cv::Point3f((length / 2), (width / 2), 0));
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -125,34 +177,64 @@ bool Map::CheckReachedDestination() {
  * based on if they are within the bounds of map or not.
  * Use transformation matrices to translate and rotate points of rectangle
  * and then determine if they are within bounds.
- * @param Point3f input coordinates to be checkes if they are valid or not
+ * @param Point3f input coordinates to be checks if they are valid or not
  * @return bool true or false depending on if the coordinates are
  * within bounds or not
  */
 bool Map::CheckValidCoordinates(cv::Point3f inputCoordinates) {
-  float tempX = inputCoordinates.x;
-  float tempY = inputCoordinates.y;
+  cv::Point2f center(inputCoordinates.x, inputCoordinates.y);
+  cv::Size2f robotSize(width, length);
+  float angle = inputCoordinates.z;
+  /// creating a rotated rectangle for given parameters
+  cv::RotatedRect rotatedRectangle = cv::RotatedRect(center, robotSize, angle);
 
-  return false;
+  std::vector<cv::Point2f> rectPoints;
+  std::vector<cv::Point2f> rotatedPoints;
+  rectPoints.push_back(cv::Point2f(-length / 2, -width / 2));
+  rectPoints.push_back(cv::Point2f(length / 2, -width / 2));
+  rectPoints.push_back(cv::Point2f(length / 2, width / 2));
+  rectPoints.push_back(cv::Point2f(-length / 2, width / 2));
+  cv::Point2f translationPoint;
+  translationPoint.x = center.x * cos(angle) + center.y * sin(angle);
+  translationPoint.y = -center.x * sin(angle) + center.y * cos(angle);
+
+  for (auto p : rectPoints) {
+    cv::Point2f rotPoint;
+    rotPoint.x = p.x * cos(angle) - p.y * sin(angle) + center.x;
+    rotPoint.y = p.x * sin(angle) + p.y * cos(angle) + center.y;
+    rotatedPoints.push_back(rotPoint);
+  }
+
+  std::cout << "Rotated points: " << rotatedPoints;
+  for (auto p : rotatedPoints) {
+    std::cout << (p.y > mapBounds.y);
+    std::cout << (p.x > mapBounds.x);
+    if (p.x < 0 || p.y < 0 || p.x > mapBounds.x || p.y > mapBounds.y) {
+      std::cout << "COndition failed " << std::endl;
+      return false;
+    }
+  }
+  return true;
+
 }
 
 /**
  * @brief Function to display map with the
  * updated location of the robot
  * @param none
- * @return bool true or false depnding on map is successfully displayed
+ * @return bool true or false depending on map is successfully displayed
  * or not.
  */
 bool Map::DisplayMapImage() {
-  try {
+  if (currMapImage.rows > 0 && currMapImage.cols > 0) {
     /// Create a window for display.
     cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
-    /// Show our image inside it
+    /// Show the image inside it
     cv::imshow("Display window", currMapImage);
     cv::waitKey(0);
     return true;
-  } catch (cv::Exception& e) {
-    std::cout << "Exception: failed to display image." << e.what();
+  } else {
+    std::cout << "Exception: failed to display image.";
     return false;
   }
 }
